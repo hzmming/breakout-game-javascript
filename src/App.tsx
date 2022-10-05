a61cf622d7971c6c44486d5331a448265f87a321
黄振明 <hzm@meitu.com>
2022-10-05 11:55:25 +0800

    feat: add category

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import { useState } from 'react';
 import './App.css';
 import PureCanvas from './components/PureCanvas';
+import UsingPhaser from './components/UsingPhaser';
 
 function App() {
+  const list = [
+    {
+      label: 'pure canvas',
+      component: PureCanvas,
+    },
+    {
+      label: 'using Phaser',
+      component: UsingPhaser,
+    },
+  ];
+  type Item = typeof list[number];
+  const [selected, setSelected] = useState<Item>(list[0]);
+
+  function show(item: Item) {
+    setSelected(item);
+  }
+
   return (
     <div>
-      <PureCanvas />
+      <ul className="category">
+        {list.map((i) => (
+          <li
+            className={selected.label === i.label ? 'selected' : ''}
+            onClick={() => show(i)}
+            key={i.label}
+          >
+            {i.label}
+          </li>
+        ))}
+      </ul>
+      {selected && <selected.component />}
     </div>
   );
 }
---
+++ src/components/UsingPhaser.tsx
@@ -0,0 +1,5 @@
+function UsingPhaser() {
+  return <div>hello</div>;
+}
+
+export default UsingPhaser;
